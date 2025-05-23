593477369c322d7c45ed369232ab00bdcb9e13f9
adilHBS <adilhussainbarari@gmail.com>
2025-05-24 00:40:34 +0500

    Move React and ReactDOM imports to the top and render App component in index file

--- timer-project/src/App.jsx
+++ timer-project/src/App.jsx
@@ -1,3 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.html'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
 import { useState, useRef } from 'react';
 import './App.css';
